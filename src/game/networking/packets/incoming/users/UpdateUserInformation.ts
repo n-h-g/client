3bbec656b88f283b130f22de0d17f0e189de5dd1
Cosimo Celeste <gogliacosimoceleste@gmail.com>
2022-12-29 10:56:01 +0100

    Removed console.log from UpdateUserInformation

--- src/game/networking/packets/incoming/users/UpdateUserInformation.ts
+++ src/game/networking/packets/incoming/users/UpdateUserInformation.ts
@@ -3,7 +3,5 @@ import { MessageHandler } from '../../../handler/MessageHandler';
 export class UpdateUserInformation extends MessageHandler {
     public handle(): void {
         let userInfo = this.message
-
-        console.log(userInfo)
     }
 }
