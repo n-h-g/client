0671bb866d6945d9592c22d1ca144c4911571eeb
Cosimo Celeste <gogliacosimoceleste@gmail.com>
2023-01-01 07:07:30 +0100

    Small changes

--- src/game/networking/packets/incoming/rooms/items/AddItem.ts
+++ src/game/networking/packets/incoming/rooms/items/AddItem.ts
@@ -15,17 +15,16 @@ export default class AddItem extends MessageHandler {
 
         let currentRoom: Room | null | undefined = Engine.getInstance().roomService?.CurrentRoom;
 
-        if(!currentRoom) return;
+        if (!currentRoom) return;
 
-        if(currentRoom.roomItemRepository.has(itemData.id)) return;
+        if (currentRoom.roomItemRepository.has(itemData.id)) return;
 
         let type: ItemType = itemData.item_type as ItemType
-            Engine.getInstance().userInterfaceManager?.furniImager.loadFurniSprite(type, itemData.baseName).then((sprite: FurniSprite) => {
-                sprite.start()
-                let item = type === ItemType.FloorItem ? new FloorItem(null, itemData.id, itemData.name, new Point3d(itemData.x, itemData.y, itemData.y), sprite) : new WallItem(null, itemData.id, itemData.name, new Point3d(itemData.x, itemData.y, itemData.z), sprite);
-                currentRoom?.roomItemRepository.add(item.id, item);
-                (item.visualization as ItemVisualization)?.updatePosition()
-            }).catch((err) => console.log(err));
-
+        Engine.getInstance().userInterfaceManager?.furniImager.loadFurniSprite(type, itemData.baseName).then((sprite: FurniSprite) => {
+            sprite.start()
+            let item = type === ItemType.FloorItem ? new FloorItem(null, itemData.id, itemData.name, new Point3d(itemData.x, itemData.y, itemData.y), sprite) : new WallItem(null, itemData.id, itemData.name, new Point3d(itemData.x, itemData.y, itemData.z), sprite);
+            currentRoom?.roomItemRepository.add(item.id, item);
+            (item.visualization as ItemVisualization)?.updatePosition()
+        }).catch((err) => console.log(err));
     }
 }
