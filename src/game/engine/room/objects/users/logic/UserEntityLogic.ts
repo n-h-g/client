08950c65aa377f61afaae9cefdb9913a8585d76b
258ms <valeriotriolo3010@gmail.com>
2023-01-14 12:16:31 +0100

    refactoring

--- src/game/engine/room/objects/users/logic/UserEntityLogic.ts
+++ src/game/engine/room/objects/users/logic/UserEntityLogic.ts
@@ -62,7 +62,7 @@ export default class UserEntityLogic extends HumanLogic {
 
     private toggleUI() {
         
-        (Engine.getInstance().userInterfaceManager.componentsManager.getComponent(UIComponent.AvatarContainerUI) as IComponentShowableUI).show()
+        (Engine.getInstance().userInterfaceManager.componentsManager.getComponent(UIComponent.AvatarContainerUI) as IComponentShowableUI).()
 
         let dimension = new Point(this.entity.visualization?.container?.height!,
             this.entity.visualization?.container?.width!
